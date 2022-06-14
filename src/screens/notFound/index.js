1ac1f995298c06d4af79a4226e70a377ef930d87
Mateus <mateus.gotardi@gmail.com>
2022-06-13 23:43:26 -0300

    redirect to home in 404 pages

--- src/screens/notFound/index.js
+++ src/screens/notFound/index.js
@@ -1,15 +1,9 @@
 import '../../styles/home.scss'
-import { Link } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 
 const NotFound = ()=>{
     return(
-        <div className="homeContainer">
-        <div className="title">
-            <h1>404 - Página não encontrada</h1>
-        </div>
-        
-        <Link to='/inicial' className="buttonHome">Home</Link>
-    </div>
+    <Navigate to="/home" />
     )
 }
 export default NotFound
